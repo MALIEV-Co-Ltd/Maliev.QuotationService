19a9a3d167db751a129ccdd9b974e993543db2f4
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-01-03 22:44:58 +0700

    feat(geometry): Add migration for FileReference geometry metrics

--- Maliev.QuotationService.Data/Migrations/20260103154444_AddGeometryMetricsToFileReference.Designer.cs
+++ Maliev.QuotationService.Data/Migrations/20260103154444_AddGeometryMetricsToFileReference.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Maliev.QuotationService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Maliev.QuotationService.Data.Migrations
 {
     [DbContext(typeof(QuotationDbContext))]
-    partial class QuotationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260103154444_AddGeometryMetricsToFileReference")]
+    partial class AddGeometryMetricsToFileReference
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
