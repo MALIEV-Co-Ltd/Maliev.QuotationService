a25a6d9576c2c6422660ace65dbcc0a1ebbf45d0
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-01-14 18:21:44 +0700

    chore: cleanup bootstrap logging and suppress CA1848 consistently

--- Maliev.QuotationService.Api/Program.cs
+++ Maliev.QuotationService.Api/Program.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CA1848 // For improved performance, use the LoggerMessage delegates
 using System.Threading.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using Maliev.QuotationService.Api.Configuration.Extensions;
